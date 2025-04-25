0111187198edb0365803c9b9f7a2542f6f8995e2
Pistold <119545844+Pistold@users.noreply.github.com>
2025-04-25 13:10:16 -0700

    Create main.py
    add api call

---
+++ main.py
@@ -0,0 +1,9 @@
+import openai
+
+openai.api_key = ""
+
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": prompt}]
+        )
